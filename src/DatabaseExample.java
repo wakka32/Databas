4a1fb2ac439742796984ea80951b28e46b4b528d
william.grenback <william.grenback@elev.ga.ntig.se>
2020-03-10 11:12:33 +0100

    Initial commit

---
+++ src/DatabaseExample.java
@@ -0,0 +1,46 @@
+/**
+ * This is a class
+ * Created 2020-03-10
+ *
+ * @author Magnus Silverdal
+ */
+import java.sql.*;
+
+public class DatabaseExample {
+    public static void main(String[] args) {
+        try {
+            // Set up connection to database
+            Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://"+DatabaseLoginData.DBURL + ":" + DatabaseLoginData.port + "/" + DatabaseLoginData.DBname +
+                            "? allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    DatabaseLoginData.user, DatabaseLoginData.password);
+
+            // Setup statement
+            Statement stmt = conn.createStatement();
+            // Create query and execute
+            String strSelect = "select body from story";
+            System.out.println("The SQL statement is: " + strSelect + "\n");
+
+            ResultSet rset = stmt.executeQuery(strSelect);
+
+            // Loop through the result set and print
+
+            System.out.println("The records selected are:");
+            int rowCount = 0;
+            while(rset.next()) {
+                String body = rset.getString("body");
+
+                System.out.println(body );
+                ++rowCount;
+            }
+            System.out.println("Total number of records = " + rowCount);
+
+            // Close conn and stmt
+            conn.close();
+            stmt.close();
+        } catch(SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
+
---
+++ src/DatabaseLoginData.java
@@ -0,0 +1,8 @@
+public class DatabaseLoginData {
+
+    public static final String DBURL = "localhost";
+    public static final String DBname = "soloäventyr";
+    public static String port = "3306";
+    public static String user = "william1";
+    public static String password = "gobananas1";
+}
